2c1bdb0ce2a8c7c555cf373f12e9686a24143926
Fikri Alfaraby <100327546+FikriAlfaraby@users.noreply.github.com>
2024-11-16 18:15:18 +0700

    🐛 add empty data line

--- src/components/complexes/dashboard/consumption-history.tsx
+++ src/components/complexes/dashboard/consumption-history.tsx
@@ -2,9 +2,22 @@
 
 import { Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 import { useUserJourney } from '../user-journey/service/fetchUserJourney.service';
+import Image from 'next/image';
 
 export function ConsumptionHistory({userId} : {userId : number}) {
   const { data: userJourneyResponse} = useUserJourney(userId);
+
+  if(!userJourneyResponse?.data.length) return (
+    <div className='flex flex-col justify-center items-center h-[340px]'>
+      <Image 
+        src="/assets/images/empty-line.svg"
+        alt="Start your journey"
+        width={180}
+        height={180}
+        className="mx-auto mb-4" />
+      <div className="">No data available</div>
+    </div>
+  )
 
   
   return (
